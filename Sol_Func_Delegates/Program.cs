4470c0d586549a53d7afe0d03949c20e171f54b8
Kishor Naik <kishor.naik011.net@gmail.com>
2019-06-19 12:19:23 +0530

    Code Commit

---
+++ Sol_Func_Delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sol_Func_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Func<String,String> funcObj= (leStr) =>{
+
+                return leStr.ToUpper();
+
+            };
+
+            Console.WriteLine(funcObj("kishor naik"));
+
+            var funcDemoObj=new FuncDemo();
+                funcDemoObj.FuncParameter("kishor naik",(leStr)=> leStr.ToUpper());
+                funcDemoObj.FuncParameter("KISHOR NAIK",(leStr)=> leStr.ToLower());
+
+                funcDemoObj.FuncParameter("Eshaan Naik",funcDemoObj.FuncReturn());
+        }
+    }
+
+    public class FuncDemo{
+
+        public void FuncParameter(String value, Func<String,String> funcStringOps){
+
+            Console.WriteLine(funcStringOps(value));
+        }
+
+        public Func<String,String> FuncReturn(){
+            return 
+                (leStr)=> $"Hello {leStr}";
+        }
+
+    }
+
+    
+}
